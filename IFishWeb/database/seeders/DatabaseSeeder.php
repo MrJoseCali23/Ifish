c31861e99c11618a9e1eeb9712d15d57d5326e0d
JOSE CALLISAYA <mrjosecallisaya@gmail.com>
2025-06-30 00:55:25 -0400

    Update DatabaseSeeder.php

--- IFishWeb/database/seeders/DatabaseSeeder.php
+++ IFishWeb/database/seeders/DatabaseSeeder.php
@@ -2,23 +2,37 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin iFISH',
+                'email' => 'admin@ifish.app',
+                'password' => Hash::make('123456'), // La contraseña será "password"
+                'rol' => 'Admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Carlos Ribera',
+                'email' => 'carlos@ifish.app',
+                'password' => Hash::make('123456'), // La contraseña será "password"
+                'rol' => 'Trabajador',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
