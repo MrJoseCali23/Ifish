2ba8a8b967e4fc96d60ec543d1656c4aa1dfa6df
JOSE CALLISAYA <mrjosecallisaya@gmail.com>
2025-09-13 20:10:33 -0400

    Agregar uso de Auth en TipoComidaController

--- IFishWeb/app/Http/Controllers/TipoComidaController.php
+++ IFishWeb/app/Http/Controllers/TipoComidaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\TipoComida;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 class TipoComidaController extends Controller
 {
     /**
